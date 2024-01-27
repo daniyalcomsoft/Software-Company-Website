550db8281d3750f3697de60868579b8c199e4cec
nazish iqbal <nazish.iqbal@thankyo.net>
2024-01-28 03:22:33 +0500

    website commit parashoot

---
+++ assets/js/layout.js
@@ -0,0 +1,31 @@
+$(function(){
+    $(".header-area").load("header.html"); 
+    $(".footer-area").load("footer.html"); 
+});
+
+// function showMobileMenu(){
+//     $('.main-nav').toggleClass('slidenav');
+// }
+// function HideMobileMenu(){
+//     $('.main-nav').toggleClass('slidenav');
+// }
+// class SiteHeader extends HTMLElement {
+//     connectedCallback() {
+//       this.innerHTML = `
+            
+//       `;
+//     }
+//   }
+      
+// customElements.define('site-header', SiteHeader);
+
+
+// class SiteFooter extends HTMLElement {
+//     connectedCallback() {
+//       this.innerHTML = `
+         
+//       `;
+//     }
+//   }
+      
+//   customElements.define('site-footer', SiteFooter);
